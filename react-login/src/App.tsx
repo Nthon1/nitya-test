1a02358298bd4407e8082748c7390616a2a6a234
JJ-Noirfos <53491467+JJ-Chang@users.noreply.github.com>
2022-12-09 20:47:13 -0500

    Initial commit

---
+++ react-login/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Auth from "./Auth"
+
+function App() {
+  return (
+      <div className="App">
+          <p>test</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </BrowserRouter>
+      </div>
+  );
+}
+
+export default App;
